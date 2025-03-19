31c303b30bb402215cd0bd64c29e1d6dac2990ca
FloatingComet62 <shubh2007rai@gmail.com>
2025-03-19 21:52:01 +0530

    cleaned up main.py

--- main.py
+++ main.py
@@ -1,103 +1,13 @@
-import fastf1
-import fastf1.plotting
-import pickle
-from team import TeamBuilder
-from helper import avg, variance, str_session_data
+from fastf1 import Cache
+from session import Session
 from rating import RatingManager
 
-RATING_FILE = "rating.dat"
-SESSION = (2024, 21, 'R')
-SPACE_DATA_FILE = f"space_data_{str_session_data(SESSION)}.dat"
+rating_manager = RatingManager("rating.dat")
+Cache.set_disabled()
+session = Session((2024, 21, 'R'))
+session.calculate_lap_scores()
+session.run_elo_round_for_lap_scores(rating_manager)
+session.run_elo_round_for_results(rating_manager)
 
-rating_manager = RatingManager(RATING_FILE)
-session = fastf1.get_session(*SESSION)
-session.load()
-
-
-def position_to_points(pos):
-    return ([25, 18, 15, 12, 10, 8, 6, 4, 2, 1] + [0] * 10)[int(pos - 1)]
-
-
-def pace_score(drive_score, driver_space):
-    return drive_score * (1 - driver_space**1/3)
-
-
-team_builders = {}
-for driver_num in session.drivers:
-    driver = session.get_driver(driver_num)
-    team_id = driver["TeamId"]
-
-    if team_id not in team_builders:
-        team_builders[team_id] = TeamBuilder(session, team_id)
-    team_builders[team_id].add_driver(driver)
-
-teams = {}
-for key in team_builders.keys():
-    teams[key] = team_builders[key].build()
-
-session_laps = []
-for i in range(session.total_laps + 1):
-    session_laps.append(session.laps[session.laps["LapNumber"] == i])
-
-try:
-    with open(SPACE_DATA_FILE, "rb") as f:
-        spaces_data = pickle.load(f)
-except Exception as _:
-    print("Spaces Cache not found")
-    spaces_data = {}
-
-laps = {}
-scores = []
-for team in teams.values():
-    achieved_time = team.get_achieved_time()
-    scores.clear()
-    for (lap, driver1_time) in team.driver1.lap_time():
-        drive_score = achieved_time / driver1_time
-        scores.append(drive_score)
-        driver_space = team.driver1.get_space(spaces_data, session_laps[lap], lap)
-        send_data = (team.driver1.id, pace_score(drive_score, driver_space))
-        if lap not in laps:
-            laps[lap] = [send_data]
-            continue
-        laps[lap].append(send_data)
-    if len(scores) != 0:
-        score_avg = avg(scores)
-        v_avg = variance(scores) * score_avg
-        print(f"{team.driver1.id} analyzed [avg: {score_avg:.2%}, variance: {v_avg:.3%}]")
-    scores.clear()
-    for (lap, driver2_time) in team.driver2.lap_time():
-        drive_score = achieved_time / driver2_time
-        scores.append(drive_score)
-        driver_space = team.driver2.get_space(spaces_data, session_laps[lap], lap)
-        send_data = (team.driver2.id, pace_score(drive_score, driver_space))
-        if lap not in laps:
-            laps[lap] = [send_data]
-            continue
-        laps[lap].append(send_data)
-    if len(scores) == 0:
-        continue
-    score_avg = avg(scores)
-    v_avg = variance(scores) * score_avg
-    print(f"{team.driver2.id} analyzed [avg: {score_avg:.2%}, variance: {v_avg:.3%}]")
-
-for (lap, net_score) in laps.items():
-    net_score.sort(key=lambda x: x[1], reverse=True)
-    scores = list(map(lambda x: x[1], net_score))
-    #rating_manager.send_lap_scores([*net_score])
-    print(f"{lap}: {net_score[0][0]} ({net_score[0][1]:.2%}) (net: {avg(scores):.2%})")
-
-print(session.results)
-for team in teams.values():
-    driver1_points = position_to_points(team.driver1.data["Position"])
-    driver2_points = position_to_points(team.driver2.data["Position"])
-    diff = abs(driver1_points - driver2_points)
-    if driver1_points > driver2_points:
-        rating_manager.send_result_scores(team.driver1.id, team.driver2.id, diff)
-    else:
-        rating_manager.send_result_scores(team.driver2.id, team.driver1.id, diff)
-
-with open(SPACE_DATA_FILE, "wb") as f:
-    pickle.dump(spaces_data, f)
-    print("Cache saved")
-
+session.deinit()
 rating_manager.deinit()
---
+++ session.py
@@ -0,0 +1,96 @@
+from fastf1 import get_session
+from team import TeamBuilder
+from pickle import load as binary_load, dump as binary_dump
+from helper import avg, variance, str_session_data
+
+
+def position_to_points(pos):
+    return ([25, 18, 15, 12, 10, 8, 6, 4, 2, 1] + [0] * 10)[int(pos - 1)]
+
+
+def pace_score(drive_score, driver_space):
+    return drive_score * (1 - driver_space**1/3)
+
+
+class Session:
+    def __init__(self, session_data):
+        session = get_session(*session_data)
+        session.load()
+        self.space_data_file = f"space_data_{str_session_data(session_data)}.dat"
+
+        team_builders = {}
+        for driver_num in session.drivers:
+            driver = session.get_driver(driver_num)
+            team_id = driver["TeamId"]
+
+            if team_id not in team_builders:
+                team_builders[team_id] = TeamBuilder(session, team_id)
+            team_builders[team_id].add_driver(driver)
+
+        self.teams = {}
+        for key in team_builders.keys():
+            self.teams[key] = team_builders[key].build()
+
+        self.session_laps = []
+        for i in range(session.total_laps + 1):
+            self.session_laps.append(session.laps[session.laps["LapNumber"] == i])
+
+        try:
+            with open(self.space_data_file, "rb") as f:
+                self.spaces_data = binary_load(f)
+        except Exception as _:
+            print("Spaces Cache not found")
+            self.spaces_data = {}
+
+    def calculate_lap_score(self, driver, achieved_time):
+        for (lap, driver1_time) in driver.lap_time():
+            drive_score = achieved_time / driver1_time
+            driver_space = driver.get_space(
+                self.spaces_data,
+                self.session_laps[lap],
+                lap
+            )
+            send_data = (driver.id, pace_score(drive_score, driver_space))
+            if lap not in self.laps:
+                self.laps[lap] = [send_data]
+                continue
+            self.laps[lap].append(send_data)
+
+    def calculate_lap_scores(self):
+        self.laps = {}
+        for team in self.teams.values():
+            achieved_time = team.get_achieved_time()
+            self.calculate_lap_score(team.driver1, achieved_time)
+            self.calculate_lap_score(team.driver2, achieved_time)
+
+    def run_elo_round_for_lap_scores(self, rating_manager):
+        assert self.laps
+
+        for (lap, net_score) in self.laps.items():
+            net_score.sort(key=lambda x: x[1], reverse=True)
+            scores = list(map(lambda x: x[1], net_score))
+            rating_manager.send_lap_scores([*net_score])
+            print(f"{lap}: {net_score[0][0]} ({net_score[0][1]:.2%}) (net: {avg(scores):.2%})")
+
+    def run_elo_round_for_results(self, rating_manager):
+        for team in self.teams.values():
+            driver1_points = position_to_points(team.driver1.data["Position"])
+            driver2_points = position_to_points(team.driver2.data["Position"])
+            diff = abs(driver1_points - driver2_points)
+            if driver1_points > driver2_points:
+                rating_manager.send_result_scores(
+                    team.driver1.id,
+                    team.driver2.id,
+                    diff
+                )
+                continue
+            rating_manager.send_result_scores(
+                team.driver2.id,
+                team.driver1.id,
+                diff
+            )
+
+    def deinit(self):
+        with open(self.space_data_file, "wb") as f:
+            binary_dump(self.spaces_data, f)
+            print("Cache saved")
